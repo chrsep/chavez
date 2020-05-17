1be7d7a668ef84eb9d70a5aa0c9a080d2c0ef379
Chrisando Pramudhita <chrsep@protonmail.com>
2020-05-17 21:51:14 +0700

    Fix config not applied.

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -8,7 +8,7 @@ config :chavez, Chavez.Repo,
   pool_size: 15
 
 port = String.to_integer(System.get_env("PORT") || "8080")
-config :chavez, Chavez.Endpoint,
+config :chavez, ChavezWeb.Endpoint,
   http: [port: port],
   url: [host: System.get_env("HOSTNAME"), port: port],
   root: ".",
