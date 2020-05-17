ba27fae3de9e1a16c461f17ffa68c55c78b29c54
Chrisando Pramudhita <chrsep@protonmail.com>
2020-05-18 02:52:58 +0700

    Update deps.

--- mix.exs
+++ mix.exs
@@ -38,12 +38,12 @@ defmodule Chavez.MixProject do
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix_html, "~> 2.14"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.18"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.1"},
-      {:distillery, "~> 2.0"},
+      {:jason, "~> 1.2"},
+      {:plug_cowboy, "~> 2.2"},
+      {:distillery, "~> 2.1"},
       {:phoenix_live_view, "~> 0.12.1"},
       {:floki, ">= 0.0.0", only: :test}
     ]
