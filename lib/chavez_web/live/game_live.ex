811e0b06e9f461cf162741b124de34ae10d38bfc
Chrisando Pramudhita <chrsep@protonmail.com>
2020-05-18 20:29:39 +0700

    Try making counter.

--- lib/chavez_web/live/game_live.ex
+++ lib/chavez_web/live/game_live.ex
@@ -1,7 +1,16 @@
 defmodule ChavezWeb.GameLive do
   use Phoenix.LiveView
 
+
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :temperature, 20)}
+    {:ok, assign(socket, :value, 1)}
+  end
+
+  def handle_event("inc", _value, socket) do
+    {:noreply, update(socket, :value, &(&1 + 1))}
+  end
+
+  def handle_event("dec", _value, socket) do
+    {:noreply, update(socket, :value, &(&1 - 1))}
   end
 end
